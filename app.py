768b2ff3fc284b3771d3e6af9c67885e74b10da9
Guilherme <guilherme.morais.54540218@gmail.com>
2026-06-17 12:12:05 -0300

    adicionei o redis nas 2 entidades

--- app.py
+++ app.py
@@ -1,11 +1,15 @@
+import json
 from flask import Flask, jsonify, request
 from flask_sqlalchemy import SQLAlchemy
+import redis
 
 app = Flask(__name__)
 
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///blog.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
+
+redis_client = redis.Redis(host='localhost', port=6379, db=0, decode_responses=True)
 
 class Autor(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -47,29 +51,54 @@ with app.app_context():
 
 @app.route('/autores/<int:autor_id>/posts', methods=['GET'])
 def listar_posts_por_autor(autor_id):
-    print("Buscando posts no banco de dados relacional...")
-    
+    redis_key = f"autor:{autor_id}:posts"
+
+    cache_posts = redis_client.get(redis_key)
+    if cache_posts:
+        print("[REDIS] Retornando posts direto do Redis...")
+        dados_cache = json.loads(cache_posts)
+        return jsonify({
+            "autor": dados_cache["autor"],
+            "posts": dados_cache["posts"]
+        })
+
+    print("[SQL] Buscando posts no banco de dados relacional...")
     autor = Autor.query.get(autor_id)
     if not autor:
         return jsonify({"erro": "Autor não encontrado"}), 404
 
     lista_posts = [post.to_dict() for post in autor.posts]
-
-    return jsonify({
-        "fonte": "Banco de Dados SQL",
+    
+    estrutura_retorno = {
         "autor": autor.nome,
         "posts": lista_posts
+    }
+
+    redis_client.set(redis_key, json.dumps(estrutura_retorno), ex=300)
+    print("Dados salvos no Redis para as próximas requisições.")
+
+    return jsonify({
+        **estrutura_retorno
     })
 
 @app.route('/autores', methods=['GET'])
 def listar_todos_autores():
-    print("Buscando todos os autores no banco de dados...")
+    redis_key = "autores:todos"
+
+    cache_autores = redis_client.get(redis_key)
+    if cache_autores:
+        print("[REDIS] Retornando lista de autores do Redis...")
+        return jsonify({
+            "autores": json.loads(cache_autores)
+        })
+
+    print("[SQL] Buscando todos os autores no banco...")
     autores = Autor.query.all()
-    
     lista_autores = [autor.to_dict() for autor in autores]
+
+    redis_client.set(redis_key, json.dumps(lista_autores), ex=300)
     
     return jsonify({
-        "fonte": "Banco de Dados SQL",
         "autores": lista_autores
     })
 
@@ -91,11 +120,14 @@ def criar_post():
     db.session.add(novo_post)
     db.session.commit()
 
+    redis_key = f"autor:{autor_id}:posts"
+    redis_client.delete(redis_key)
+    print(f"[REDIS] Cache limpo para a chave: {redis_key} devido a um novo post.")
+
     return jsonify({
         "mensagem": "Post criado com sucesso!",
         "post": novo_post.to_dict()
     }), 201
-
 
 if __name__ == '__main__':
     app.run(debug=True)
